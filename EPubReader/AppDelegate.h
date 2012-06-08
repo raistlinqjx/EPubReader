c594341fc77c62c7983c57e93c3855cd9b5ce53e
QJX apple <qjxapple@QJX-applematoMacBook-Pro.local>
2012-06-08 09:48:55 +0800

    Initial Commit

---
+++ EPubReader/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  EPubReader
+//
+//  Created by apple QJX on 12-6-8.
+//  Copyright (c) 2012年 HZNetquick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ EPubReader/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  EPubReader
+//
+//  Created by apple QJX on 12-6-8.
+//  Copyright (c) 2012年 HZNetquick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
